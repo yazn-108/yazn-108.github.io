84664d9f8131d19d3b70ffbc2e8db49548efe18e
yazn_108 <102805404+yazn-108@users.noreply.github.com>
2023-07-07 02:51:31 +0300

    @yazn_108

--- style/script.js
+++ style/script.js
@@ -1,23 +1,24 @@
 "use strict";
-let skills_img = document.querySelectorAll(".skills img");
-skills_img[1].setAttribute("src","imgs/github.svg");
-skills_img[2].setAttribute("src","imgs/html.svg");
-skills_img[3].setAttribute("src","imgs/css.svg");
-skills_img[4].setAttribute("src","imgs/git.svg");
-skills_img[5].setAttribute("src","imgs/javascript.svg");
-skills_img[6].setAttribute("src","imgs/figma.svg");
+let skillsImg = document.querySelectorAll(".skills .box img");
+let skillsArray = [
+    "imgs/github.svg",
+    "imgs/html.svg",
+    "imgs/css.svg","imgs/git.svg",
+    "imgs/javascript.svg",
+    "imgs/figma.svg"];
+skillsImg.forEach((img,index) => {img.setAttribute("src",skillsArray[index]);});
 
-let menu_icon = document.querySelector(".menu-icon");
+let menuIcon = document.querySelector(".menu-icon");
 let sections = document.querySelector(".sections");
-menu_icon.onclick = function(e) {
+menuIcon.onclick = function(e) {
     e.stopPropagation();
     sections.classList.toggle("menu");
-    menu_icon.classList.toggle("close");
+    menuIcon.classList.toggle("close");
     let menuFun = function(e){
         let menu = document.querySelector(".menu");
         if(e.target !== menu){
             sections.classList.remove("menu");
-            menu_icon.classList.remove("close");
+            menuIcon.classList.remove("close");
         };
     };
 document.addEventListener("click",menuFun);
@@ -26,9 +27,11 @@ document.addEventListener("scroll",menuFun);
 let dark = document.querySelector(".dark-mode");
 dark.onclick = () => {dark.classList.toggle("dark");};
 
-fetch("https://api.github.com/users/yazn-108/repos").then(response => response.json())
-    .then(data => {
-    data.sort((a, b) => {return new Date(b.created_at) - new Date(a.created_at);});
+async function apiProjects(){
+    try {
+        const api = await fetch("https://api.github.com/users/yazn-108/repos");
+        let data = await api.json()
+        data.sort((a, b) => {return new Date(b.created_at) - new Date(a.created_at);});
         for(const loop in data){
             if(data[loop].has_pages === true && data[loop].description) {
                 let container = document.querySelector(".projects .boxes");
@@ -63,14 +66,15 @@ fetch("https://api.github.com/users/yazn-108/repos").then(response => response.j
                 description.addEventListener("mouseover",() => {
                     description.textContent = data[loop].description;
                     description.style.textAlign = "center";
-                    description.style.fontSize = "x-large";
+                    description.style.fontSize = "large";
                     description.style.padding = "0px 50px";});
                 description.addEventListener("mouseleave",() => {
                     description.textContent = repoName;
-                    description.style.fontSize = "xx-large";});
+                    description.style.fontSize = "x-large";});
             };
         };
-    }).catch(() =>{
+        
+    } catch (error) {
         let container = document.querySelector(".projects");
         const errorBox = document.createElement("div");
         errorBox.className = "errorBox";
@@ -83,4 +87,7 @@ fetch("https://api.github.com/users/yazn-108/repos").then(response => response.j
         let reloadButton = document.createElement("button");
         reloadButton.textContent = "Reload the page";
         reloadPage.appendChild(reloadButton);
-        container.appendChild(errorBox);});
+        container.appendChild(errorBox);
+    };
+};    
+apiProjects();

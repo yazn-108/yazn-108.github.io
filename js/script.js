fe5b855f65549d3a19345823cba1a30436308602
Ezzedine2000 <gamesalab22@gmail.com>
2023-11-14 01:39:49 +0300

    @yazn_108

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ headroom.init();
 
 let sectionLinks = document.querySelectorAll("nav .sections button");
 sectionLinks.forEach((section) => {
-    section.addEventListener("click",(e) => {
+    section.addEventListener("click", (e) => {
         if (matchMedia("(max-width: 768px)").matches) {
             sectionLinks.forEach((ele) => {
                 ele.style.textDecoration = "none";
                 e.target.style = `text-decoration: line-through var(--main-color) 2px;`;
             });
-        }else{
+        } else {
             sectionLinks.forEach((ele) => {
                 ele.style.color = "";
-                localStorage.getItem("theme_is") === "dark"?
-                e.target.style.color = "var(--body-color)"
-                :e.target.style.color = "var(--main-color)";
+                localStorage.getItem("theme_is") === "dark" ?
+                    e.target.style.color = "var(--body-color)"
+                    : e.target.style.color = "var(--main-color)";
             });
         };
         document.querySelector(e.target.dataset.section).scrollIntoView({
@@ -27,62 +27,103 @@ sectionLinks.forEach((section) => {
 
 let menuIcon = document.querySelector(".menu-icon");
 let sections = document.querySelector(".sections");
-menuIcon.addEventListener("click", function(e) {
+menuIcon.addEventListener("click", function (e) {
     e.stopPropagation();
     sections.classList.toggle("menu");
     menuIcon.classList.toggle("close");
-    let menuFun = function(e){
+    let menuFun = function (e) {
         let menu = document.querySelector(".menu");
-        if(e.target !== menu){
+        if (e.target !== menu) {
             sections.classList.remove("menu");
             menuIcon.classList.remove("close");
         };
     };
-document.addEventListener("click",menuFun);
-document.addEventListener("scroll",menuFun);
+    document.addEventListener("click", menuFun);
+    document.addEventListener("scroll", menuFun);
 });
 
-let skillsImg = document.querySelectorAll(".skills .box img");
-let skillsArray = [
-"imgs/github.svg",
-"imgs/html.svg",
-"imgs/css.svg",
-"imgs/git.svg",
-"imgs/javascript.svg",
-"imgs/figma.svg"];
-skillsImg.forEach((img,index) => {img.setAttribute("src",skillsArray[index]);});
+let skillsBox = document.querySelector(".skills .box");
+import { skillsData } from './skillsSectionData/Data.js'
+skillsData.map(info => {
+    const span = document.createElement("span");
+    span.style = `--skill-name:'${info.languageName}'; --skill-color:${info.languageColor};`
+    const img = document.createElement("img");
+    img.src = info.languageIcon
+    span.appendChild(img);
+    skillsBox.appendChild(span);
+});
+
 let colorOfDarkMode = getComputedStyle(document.documentElement).getPropertyValue('--dark-background');
 let browserTheme = document.querySelector('meta[name="theme-color"]');
 let dark = document.querySelector(".theme-mode");
 dark.addEventListener("click", () => {
-sectionLinks.forEach(ele => ele.style.color = "");
-dark.classList.toggle("dark");
-if(dark.classList.contains("dark")){
-    localStorage.setItem("theme_is", "dark");
-    browserTheme.setAttribute("content",colorOfDarkMode)
-    document.body.style.backgroundColor = colorOfDarkMode;
-}else{
-    localStorage.setItem("theme_is", "light");
-    browserTheme.setAttribute("content","")
-    document.body.style.backgroundColor = "";
-};
+    sectionLinks.forEach(ele => ele.style.color = "");
+    dark.classList.toggle("dark");
+    if (dark.classList.contains("dark")) {
+        localStorage.setItem("theme_is", "dark");
+        browserTheme.setAttribute("content", colorOfDarkMode)
+        document.body.style.backgroundColor = colorOfDarkMode;
+    } else {
+        localStorage.setItem("theme_is", "light");
+        browserTheme.setAttribute("content", "")
+        document.body.style.backgroundColor = "";
+    };
 });
-if(localStorage.getItem("theme_is") === "dark"){
+if (localStorage.getItem("theme_is") === "dark") {
     dark.classList.add(localStorage.getItem("theme_is"));
-    browserTheme.setAttribute("content",colorOfDarkMode)
+    browserTheme.setAttribute("content", colorOfDarkMode)
     document.body.style.backgroundColor = colorOfDarkMode;
 };
 
-async function apiProjects(){
+
+
+// let categorys = document.querySelector(".projects .categorys");
+// async function APICategorys() {
+//     try {
+//         const api = await fetch("https://api.github.com/users/yazn-108/repos");
+//         let data = await api.json();
+//         let categorysArray = [];
+//         data.forEach(e => categorysArray.push(e.topics));
+
+//         categorysArray.flat(Infinity).forEach(items => {
+//             let category = document.createElement("button");
+//             category.dataset.class = items
+//             category.textContent = items;
+//             categorys.appendChild(category);
+//         });
+
+//         let buttons = categorys.querySelectorAll("button")
+//         buttons.forEach(button => {
+//             button.addEventListener("click", (e) => {
+//                 let projects = document.querySelectorAll(".projects .box");
+//                 projects.forEach(e => e.style.display = "none");
+
+//                 document.querySelectorAll(`.${e.target.dataset.class}`).forEach(e => e.style.display = "block");
+
+//             });
+//         });
+
+
+
+//     } catch {
+
+//     };
+// };
+// APICategorys();
+
+
+
+
+async function apiProjects() {
     try {
         const api = await fetch("https://api.github.com/users/yazn-108/repos");
         let data = await api.json()
-        data.sort((a, b) => {return new Date(b.pushed_at) - new Date(a.pushed_at);});
-        for(const loop in data){
-            if(data[loop].has_pages === true && data[loop].description && data[loop].name !== "yazn-108.github.io") {
+        data.sort((a, b) => { return new Date(b.pushed_at) - new Date(a.pushed_at); });
+        for (const loop in data) {
+            if (data[loop].has_pages === true && data[loop].description && data[loop].name !== "yazn-108.github.io") {
                 let container = document.querySelector(".projects .boxes");
                 let box = document.createElement("div");
-                box.className = "box";
+                box.className = `box ${data[loop].topics}`;
                 let repoName = data[loop].name.replace(/[^a-zA-Z0-9\s]/g, " ").toLowerCase();
                 let description = document.createElement("div");
                 description.className = "description";
@@ -92,14 +133,14 @@ async function apiProjects(){
                 let repoLink = data[loop].html_url;
                 const sourceLink = document.createElement("a");
                 sourceLink.href = repoLink;
-                sourceLink.setAttribute("target","_blank");
+                sourceLink.setAttribute("target", "_blank");
                 const sourceButton = document.createElement("button");
                 sourceButton.textContent = "source";
                 sourceLink.appendChild(sourceButton);
                 let siteLink = data[loop].homepage;
                 const browsingLink = document.createElement("a");
                 browsingLink.href = siteLink;
-                browsingLink.setAttribute("target","_blank");
+                browsingLink.setAttribute("target", "_blank");
                 const browseButton = document.createElement("button");
                 browseButton.className = "browse-button";
                 browseButton.textContent = "browse";
@@ -109,13 +150,13 @@ async function apiProjects(){
                 info.appendChild(sourceLink);
                 info.appendChild(browsingLink);
                 container.appendChild(box);
-                description.addEventListener("mouseenter",() => {
+                description.addEventListener("mouseenter", () => {
                     description.textContent = data[loop].description;
                     description.style.textAlign = "center";
                     description.style.fontSize = "large";
                     description.style.padding = "0px 50px";
                 });
-                description.addEventListener("mouseleave",() => {
+                description.addEventListener("mouseleave", () => {
                     description.textContent = repoName;
                     description.style.fontSize = "xx-large";
                 });
@@ -136,40 +177,40 @@ async function apiProjects(){
         reloadPage.appendChild(reloadButton);
         container.appendChild(errorBox);
     };
-};    
+};
 apiProjects();
 
 let moreProjects = document.querySelector(".moreProjects");
-moreProjects.addEventListener("click",(e) => e.currentTarget.parentNode.classList.toggle("open"));
+moreProjects.addEventListener("click", (e) => e.currentTarget.parentNode.classList.toggle("open"));
 
 let formTitle = document.querySelector(".contact h3");
 let form = document.querySelector(".contact form");
 let emailInput = document.querySelector(".contact input[type='email']");
 let sendButton = document.querySelector(".contact input[type='submit']");
 let inputs = document.querySelectorAll("form .required");
-sendButton.addEventListener("click",(e) => {
+sendButton.addEventListener("click", (e) => {
     e.preventDefault();
     let count = 0
     inputs.forEach(input => {
-        input.value !== ""?count++
-        :input.placeholder = 'fill in the field';
+        input.value !== "" ? count++
+            : input.placeholder = 'fill in the field';
     });
-    if(count === 3 && validator.isEmail(emailInput.value) === true){
+    if (count === 3 && validator.isEmail(emailInput.value) === true) {
         formTitle.innerHTML = 'Sending...';
         const serviceID = 'default_service';
         const templateID = 'template_5ffcrsw';
         emailjs.sendForm(serviceID, templateID, form)
-        .then(() => {
-            formTitle.innerHTML = 'contact with me';
-            inputs[0].value = ""
-            inputs[1].value = ""
-            inputs[2].value = ""
-            inputs[0].placeholder = "your name"
-            inputs[1].placeholder = "your email"
-            inputs[2].placeholder = "your message"
-        }, (err) => {
-            formTitle.innerHTML = 'an error occurred';
-        });
+            .then(() => {
+                formTitle.innerHTML = 'contact with me';
+                inputs[0].value = ""
+                inputs[1].value = ""
+                inputs[2].value = ""
+                inputs[0].placeholder = "your name"
+                inputs[1].placeholder = "your email"
+                inputs[2].placeholder = "your message"
+            }, (err) => {
+                formTitle.innerHTML = 'an error occurred';
+            });
     }
 });
 emailjs.init('moFv9CUybtNEEmMkC');
---
+++ js/skillsSectionData/Data.js
@@ -0,0 +1,32 @@
+export const skillsData = [
+    {
+        languageName: "github",
+        languageIcon: "../js/skillsSectionData/github.svg",
+        languageColor: "#000000"
+    },
+    {
+        languageName: "html",
+        languageIcon: "../js/skillsSectionData/html.svg",
+        languageColor: "#E44D26"
+    },
+    {
+        languageName: "css",
+        languageIcon: "../js/skillsSectionData/css.svg",
+        languageColor: "#1572B6"
+    },
+    {
+        languageName: "git",
+        languageIcon: "../js/skillsSectionData/git.svg",
+        languageColor: "#DE4C36"
+    },
+    {
+        languageName: "javascript",
+        languageIcon: "../js/skillsSectionData/javascript.svg",
+        languageColor: "#f7df1e"
+    },
+    {
+        languageName: "figma",
+        languageIcon: "../js/skillsSectionData/figma.svg",
+        languageColor: "#6C63FF"
+    },
+]

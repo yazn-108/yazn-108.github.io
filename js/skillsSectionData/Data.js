16bc9070552322c7032ce454b4c85176713b2ed4
yazn-108 <gamesalab22@gmail.com>
2024-07-28 13:12:45 +0300

    @yazn_108

--- js/skillsSectionData/Data.js
+++ js/skillsSectionData/Data.js
@@ -1,62 +1,62 @@
 export const skillsData = [
-    {
-        languageName: "github",
-        languageIcon: "../js/skillsSectionData/github.svg",
-        languageColor: "#000000"
-    },
-    {
-        languageName: "html",
-        languageIcon: "../js/skillsSectionData/html.svg",
-        languageColor: "#E44D26"
-    },
-    {
-        languageName: "css",
-        languageIcon: "../js/skillsSectionData/css.svg",
-        languageColor: "#1572B6"
-    },
-    {
-        languageName: "git",
-        languageIcon: "../js/skillsSectionData/git.svg",
-        languageColor: "#DE4C36"
-    },
-    {
-        languageName: "javascript",
-        languageIcon: "../js/skillsSectionData/javascript.svg",
-        languageColor: "#f7df1e"
-    },
-    {
-        languageName: "figma",
-        languageIcon: "../js/skillsSectionData/figma.svg",
-        languageColor: "#6C63FF"
-    },
-    {
-        languageName: "react",
-        languageIcon: "../js/skillsSectionData/React-icon.svg",
-        languageColor: "#61dafb"
-    },
-    {
-        languageName: "vite",
-        languageIcon: "../js/skillsSectionData/vite.svg",
-        languageColor: "#ffb50d"
-    },
-    {
-        languageName: "redux",
-        languageIcon: "../js/skillsSectionData/redux.svg",
-        languageColor: "#764ABC"
-    },
-    {
-        languageName: "toolkit",
-        languageIcon: "../js/skillsSectionData/redux.svg",
-        languageColor: "#764ABC"
-    },
-    {
-        languageName: "next.js",
-        languageIcon: "../js/skillsSectionData/next-js.svg",
-        languageColor: "black"
-    },
-    {
-        languageName: "Tailwind",
-        languageIcon: "../js/skillsSectionData/tailwind.svg",
-        languageColor: "#44a8b3"
-    },
-]
+  {
+    languageName: "github",
+    languageIcon: "../js/skillsSectionData/github.svg",
+    languageColor: "#000000",
+  },
+  {
+    languageName: "html",
+    languageIcon: "../js/skillsSectionData/html.svg",
+    languageColor: "#E44D26",
+  },
+  {
+    languageName: "css",
+    languageIcon: "../js/skillsSectionData/css.svg",
+    languageColor: "#1572B6",
+  },
+  {
+    languageName: "git",
+    languageIcon: "../js/skillsSectionData/git.svg",
+    languageColor: "#DE4C36",
+  },
+  {
+    languageName: "javascript",
+    languageIcon: "../js/skillsSectionData/javascript.svg",
+    languageColor: "#f7df1e",
+  },
+  {
+    languageName: "figma",
+    languageIcon: "../js/skillsSectionData/figma.svg",
+    languageColor: "#6C63FF",
+  },
+  {
+    languageName: "react",
+    languageIcon: "../js/skillsSectionData/React-icon.svg",
+    languageColor: "#61dafb",
+  },
+  {
+    languageName: "vite",
+    languageIcon: "../js/skillsSectionData/vite.svg",
+    languageColor: "#ffb50d",
+  },
+  {
+    languageName: "redux",
+    languageIcon: "../js/skillsSectionData/redux.svg",
+    languageColor: "#764ABC",
+  },
+  {
+    languageName: "toolkit",
+    languageIcon: "../js/skillsSectionData/redux.svg",
+    languageColor: "#764ABC",
+  },
+  {
+    languageName: "next.js",
+    languageIcon: "../js/skillsSectionData/next-js.svg",
+    languageColor: "black",
+  },
+  {
+    languageName: "Tailwind",
+    languageIcon: "../js/skillsSectionData/tailwind.svg",
+    languageColor: "#44a8b3",
+  },
+];

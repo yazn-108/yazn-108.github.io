19e2f535e394c1fabdfe9fcc15e0147520476531
yazn-108 <gamesalab22@gmail.com>
2025-07-06 14:27:11 +0300

    @yazn_108

--- js/script.js
+++ js/script.js
@@ -49,10 +49,6 @@ themeButton.addEventListener("click", () => {
   const isDark = !body.classList.contains("dark");
   updateTheme(isDark);
 });
-const cv = document.getElementById("cv");
-cv.addEventListener("click", () =>
-  open("/yazn_108-cv.pdf", window.innerWidth > 768 ? "_blank" : "_self")
-);
 import { translationFile } from "./translation.js";
 const languageButton = document.querySelector(".language");
 const mainContainer = document.querySelector(".container");

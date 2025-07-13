f127ad5b279aa9a1bbe51bb61794f2892ac221ee
yazn-108 <gamesalab22@gmail.com>
2025-07-13 21:20:33 +0300

    @yazn_108

--- js/script.js
+++ js/script.js
@@ -123,8 +123,8 @@ const apiProjects = async ({ getAll }) => {
         projects += hasImage
           ? `
             <div class="font-Alkatra w-full h-[300px] grid grid-cols-1 grid-rows-[1fr_15%]">
-                <div class="group rounded-t-3xl bg-cover bg-center" style="background-image: url('https://raw.githubusercontent.com/yazn-108/${data[loop].name}/main/siteBanner.png');">
-                    <p class="opacity-0 duration-500 group-hover:opacity-100 grid place-items-center text-white px-9 size-full rounded-t-3xl backdrop-blur-md backdrop-brightness-75 text-center">
+                <div class="description-container rounded-t-3xl bg-cover bg-center" style="background-image: url('https://raw.githubusercontent.com/yazn-108/${data[loop].name}/main/siteBanner.png');">
+                    <p class="opacity-0 duration-500 description grid place-items-center text-white px-9 size-full rounded-t-3xl backdrop-blur-md backdrop-brightness-75 text-center">
                     ${data[loop].description}
                     </p>
                 </div>
@@ -139,11 +139,11 @@ const apiProjects = async ({ getAll }) => {
             </div>`
           : `
             <div class="font-Alkatra w-full h-[300px] grid grid-cols-1 grid-rows-[1fr_15%]">
-                <div class="group rounded-t-3xl bg-cover bg-center relative" style="background-image: url('/imgs/projectsBg.png');">
+                <div class="description-container rounded-t-3xl bg-cover bg-center relative" style="background-image: url('/imgs/projectsBg.png');">
                   <p class="w-full p-5 rounded-t-3xl flex justify-center items-center text-center absolute text-white">${data[
                     loop
                   ].name.replaceAll("-", " ")}</p>
-                  <p class="opacity-0 duration-500 group-hover:opacity-100 grid place-items-center text-white px-9 size-full rounded-t-3xl backdrop-blur-md backdrop-brightness-75 text-center">
+                  <p class="opacity-0 duration-500 description grid place-items-center text-white px-9 size-full rounded-t-3xl backdrop-blur-md backdrop-brightness-75 text-center">
                     ${data[loop].description}
                   </p>
                 </div>
